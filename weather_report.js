620c7da316563322a2e35de6a6c9a5f541018eb1
in4mus <in4_mus@hotmail.com>
2025-05-05 19:22:52 -0400

    Completed weather report lab

--- weather_report.js
+++ weather_report.js
@@ -1,1 +1,22 @@
 
+function showweatherDetails(event) {
+    event.preventDefault();
+
+
+    const city = document.getElementById('city').value;
+    const apiKey = '629506f8fabccdf3afc756c21eeb18cb'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+                    console.log(data);
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
